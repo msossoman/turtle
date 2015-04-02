5c1da7b2140dd83494bdc9d6c45214cc31e3d74e
Mark Sossoman <msossoman@gmail.com>
2015-04-02 17:00:40 -0400

    revised circles.py by taking out "turtle." from each line of code

--- circles.py
+++ circles.py
@@ -1,29 +1,28 @@
 from turtle import *
-turtle = Turtle()
 
-# pink circle - I can't figure out why I have to use fill() twice with the 1st circle. 
-# 				If I leave out the first fill() then the pink circle doesn't get filled in.
-turtle.color('pink')
-turtle.fill(50)
-turtle.circle(50)
-turtle.fill(50)
+# pink circle - I can't figure out why I have to use fill() twice with the 1st circle 
+# 				If I leave out the first fill() then the pink circle doesn't get filled in
+color('pink')
+fill(50)
+circle(50)
+fill(50)
 
-turtle.up()
-turtle.goto(0, -100)
-turtle.down()
+up()
+goto(0, -100)
+down()
 
 # red circle
-turtle.color('red')
-turtle.circle(50)
-turtle.fill(50)
+color('red')
+circle(50)
+fill(50)
 
-turtle.up()
-turtle.goto(0, -200)
-turtle.down()
+up()
+goto(0, -200)
+down()
 
 # green circle
-turtle.color('green')
-turtle.circle(50)
-turtle.fill(50)
+color('green')
+circle(50)
+fill(50)
 
 done()
